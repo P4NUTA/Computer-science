459bb06497156b21e8b699bcd71c45bc69e4c965
Павел <PaulEfimov2001@yandex.ru>
2019-10-24 18:28:32 +0300

    lab_3 (alexey)

--- Лаба 3/lab_03_01.py
+++ Лаба 3/lab_03_01.py
@@ -1,6 +1,4 @@
-'''
- Кортежи
-'''
+'''     Кортежи '''
 # создание кортежа
 a1 = tuple()
 a2 = 1, 2, 3, "abc"
@@ -8,26 +6,37 @@ a3 = (1, 2, 3, "abc")
 print("Tuple a1 = ", a1)
 print("Tuple a2 = ", a2)
 print("Tuple a3 = ", a3)
+
 # создание кортежа из других структур данных
 l = [1, 2, 3, "abc"]  # из списка
 a4 = tuple(l)
 print("Tuple a4 from list l = ", a4)
 a5 = tuple("Hello, World!")  # из строки
 print("Tuple a5 from string = ", a5)
+
 # вложенность кортежей
 a6 = a2, a3
 print("Tuple a6 formed by a2 and a3 = ", a6)
+
 # объединение кортежей
 a7 = a2 + a3
 print("Tuple a7 by combining a2 and a3 = ", a7)
+
 # доступ к элементам кортежей
 print("a6[0]: ", a6[0])
 print("a6[0][3]: ", a6[0][3])
-#a6[0][3] = "cba"
+a6[0][3] = "cba"  # Изменение кортежа недопустимо
 print("\n")
 
-# 3 point
-k1 = 4, 10, 2001
-k2 = "Efimov", "Pavel", "Leonidovich"
+''' 3 Задание '''
+print("Введите дату вашего рождения")
+k1 = int(input()), int(input()), int(input())
+print("Введите ваше ФИО")
+k2 = str(input()), str(input()), str(input())
 k3 = k1 + k2
-print("Tuple k3 = ", k3)
+print(k3)
+
+''' 4 Задание '''
+k4 = k1, k2
+print(k4)
+print(k4[1][1])
---
+++ Лаба 3/lab_03_07.py
@@ -0,0 +1,46 @@
+'''
+ Аргументы функции
+'''
+
+
+def sum(x, y, z=1):
+    return x + y + z
+
+
+print("sum(1,2,3): ", sum(1, 2, 3))
+print("sum(1,2): ", sum(1, 2))
+print("sum(x=1,y=3): ", sum(x=1, y=3))
+
+
+# переменное количество аргументов
+def printArgs(*args):
+    print("args of printArgs(): ", args)
+    return
+
+
+# переменное количество аргументов и аргументов-ключевых слов
+def printArgsnKwargs(m, *args, **kwargs):
+    print("main argument of printArgsnKwargs(): ", m)
+    print("args of printArgsnKwargs(): ", args)
+    print("args of printArgsnKwargs(): ", kwargs)
+    return
+
+
+printArgs("Hello World!", 1, 3, 5)
+printArgsnKwargs("Earth", 7.125, radius=6371, pos=3)
+print("\n")
+
+
+def checkArgs(*nums, **kwarg):
+    sum = 0
+    sum1 = 0
+    for n in nums:
+        sum +=1
+    for a in kwarg:
+        sum1 +=1
+    if (sum <=3) and (sum1 < 3):
+        print(*nums, **kwarg)
+    return
+
+checkArgs(12,33,"Suka","Pisda")
+
