ee94b441ae6dd94dc9d01b84b933d63f590087f9
Павел <PaulEfimov2001@yandex.ru>
2019-11-03 02:35:15 +0300

    lab_4 10 point

--- Лаба 4/lab_04_02.py
+++ Лаба 4/lab_04_02.py
@@ -12,6 +12,20 @@ class Worker:
         print("{} {}".format(self.name, self.surname))
 
 
+class Animal:
+    count = 0
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        # 9 point
+        self.id = Animal.count
+        Animal.count += 1
+
+    def display(self):
+        print("Animal {} {} {}".format(self.name, self.age, self.id))
+
+
 w1 = Worker("Ivan", "Ivanov")
 print("w1.count: ", w1.count)
 w2 = Worker("Alexei", "Petrov")
@@ -22,3 +36,13 @@ print("Worker.__name__: ", Worker.__name__)
 print("Worker.__dict__: ", Worker.__dict__)
 print("Worker.__doc__: ", Worker.__doc__)
 print("Worker.__bases__: ", Worker.__bases__)
+
+# 7 point
+# 8 point
+# id является общим атрибутом
+a0 = Animal("Ponyta", "3")
+a1 = Animal("Roselia", "5")
+a2 = Animal("Pikachu", "1")
+a0.display()
+a1.display()
+a2.display()
