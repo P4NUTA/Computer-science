dce375a16945015558688a14b52a8b6c9b818d24
Павел <PaulEfimov2001@yandex.ru>
2019-11-05 11:04:16 +0300

    lab_4 14_2 point

--- Лаба 4/lab_04_05.py
+++ Лаба 4/lab_04_05.py
@@ -29,9 +29,35 @@ class Student(Person):
         print("recordBook : ", self.recordBook)
 
 
-p1 = Student("Pavel", "Efimov", 18, [5, 2, 3, 4, 5])
-p1.display()
-p2 = Student("Aleksei", "Malyshev", 18, [5, 4, 5, 3, 4])
-p2.display()
-p3 = Student("Ksenia", "Anisimova", 18, [5, 5, 5, 4, 5])
-p3.display()
+class Professor(Person):
+    count = 0
+
+    def __init__(self, firstname, lastname, age, degree):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.age = age
+        self.id = Professor.count
+        self.degree = degree
+        Professor.count += 1
+
+    def display(self):
+        print("First name : ", self.firstname)
+        print("Last name : ", self.lastname)
+        print("Age : ", self.age)
+        print("ProfessorID : ", self.id)
+        print("Degree : ", self.degree)
+
+
+s1 = Student("Pavel", "Efimov", 18, [5, 2, 3, 4, 5])
+s1.display()
+s2 = Student("Aleksei", "Malyshev", 18, [5, 4, 5, 3, 4])
+s2.display()
+s3 = Student("Ksenia", "Anisimova", 18, [5, 5, 5, 4, 5])
+s3.display()
+
+P1 = Professor("Galina", "Li", 18, "PhD")
+P1.display()
+P2 = Professor("Vladimir", "Gololobov", 18, "PhD")
+P2.display()
+P3 = Professor("Ekaterina", "Antropova", 18, "PhD")
+P3.display()
