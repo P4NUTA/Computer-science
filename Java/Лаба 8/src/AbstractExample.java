fb1270ca0a6452fd4feafb6547e2f78a250a0f37
Павел <PaulEfimov2001@yandex.ru>
2019-12-20 21:53:44 +0300

    lab_8 not 15

--- Java/Лаба 8/src/AbstractExample.java
+++ Java/Лаба 8/src/AbstractExample.java
@@ -2,37 +2,49 @@
 abstract class Storage {
     // сохранить данные (абстрактный метод)
     public abstract void store(String data);
+
     // чтение данных (абстрактный метод)
     public abstract String read();
 }
+
 // класс, реализующий абстрактный
 class CD extends Storage {
     private String data = "";
-    public CD() {}
+
+    public CD() {
+    }
+
     @Override // реализация
     public void store(String data) {
         this.data += data;
         System.out.println("Data stored on CD");
     }
+
     @Override // реализация
     public String read() {
         return this.data;
     }
 }
+
 // класс, реализующий абстрактный
 class SDCard extends Storage {
     private String data = "";
-    public SDCard() {}
+
+    public SDCard() {
+    }
+
     @Override // реализация
     public void store(String data) {
         this.data += data;
         System.out.println("Data stored on SDCard");
     }
+
     @Override // реализация
     public String read() {
         return this.data;
     }
 }
+
 public class AbstractExample {
     public static void main(String[] args) {
         // создание объекта CD
@@ -47,11 +59,13 @@ public class AbstractExample {
         // требует реализации абстрактных методов
         Storage storage = new Storage() {
             private String data = "";
+
             @Override
             public void store(String data) {
                 this.data += data;
                 System.out.println("Data stored on storage");
             }
+
             @Override
             public String read() {
                 return this.data;
@@ -60,4 +74,34 @@ public class AbstractExample {
         storage.store("Some data to storage");
         System.out.println(storage.read());
     }
+}
+
+abstract class Gift {
+    public abstract void buy();
+
+    public abstract void give(String person);
+}
+
+class Postcard extends Gift {
+    private String person = "";
+    private String wish = "";
+
+    @Override
+    public void buy() {
+        System.out.println("Postcard is bought");
+    }
+
+    @Override
+    public void give(String person) {
+        this.person = person;
+    }
+
+    public void writeWish(String wish) {
+        this.wish = wish;
+    }
+
+    public String getWish() {
+        return this.wish;
+    }
+
 }
